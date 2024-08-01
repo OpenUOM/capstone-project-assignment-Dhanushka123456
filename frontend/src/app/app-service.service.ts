ead56a71de575d645a301fac700b44ba31b0fa13
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2024-08-01 16:14:30 +0530

    Update app-service.service.ts

--- frontend/src/app/app-service.service.ts
+++ frontend/src/app/app-service.service.ts
@@ -18,46 +18,46 @@ export class AppServiceService {
   }
 
   initializeDB(){
-    return this.http.get(`/dbinitialize`)
+    return this.http.get(`/api/dbinitialize`)
   }
 
   getTeacherData(){
-    return this.http.get('/listTeachers')
+    return this.http.get('/api/listTeachers')
   }
 
   getStudentData(){
-    return this.http.get(`/listStudents`)
+    return this.http.get(`/api/listStudents`)
   }
 
   getOneStudentData(payload: Object){
-    return this.http.post(`/getStudentInfo`, payload)
+    return this.http.post(`/api/getStudentInfo`, payload)
   }
 
   getOneTeacherData(payload: Object){
-    return this.http.post(`/getTeacherInfo`, payload)
+    return this.http.post(`/api/getTeacherInfo`, payload)
   }
 
   addTeacher(payload: Object){
-    return this.http.post(`/addTeacher`, payload)
+    return this.http.post(`/api/addTeacher`, payload)
   }
 
   deleteTeacher(payload: Object){
-    return this.http.post(`/deleteTeacher`, payload)
+    return this.http.post(`/api/deleteTeacher`, payload)
   }
 
   editTeacher(payload: Object){
-    return this.http.post(`/editTeacher`, payload)
+    return this.http.post(`/api/editTeacher`, payload)
   }
 
   editStudent(payload: Object){
-    return this.http.post(`/editStudent`, payload)
+    return this.http.post(`/api/editStudent`, payload)
   }
 
   addStudent(payload: Object){
-    return this.http.post(`/addStudent`, payload)
+    return this.http.post(`/api/addStudent`, payload)
   }
 
   deleteStudent(payload: Object){
-    return this.http.post(`/deleteStudent`, payload)
+    return this.http.post(`/api/deleteStudent`, payload)
   }
 }
