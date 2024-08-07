d447de8475a0e648125c113ff7242b467e1b4020
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2024-08-07 17:17:56 +0530

    Update teacher-add.test.js

--- frontend/test/teacher-add.test.js
+++ frontend/test/teacher-add.test.js
@@ -2,7 +2,7 @@ import {Selector} from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Teacher UI`
-    .page`http://localhost:3000/`
+    .page`http://localhost:8080/teacher`
 
 test('Testing add teachers', async t => {
     await t.navigateTo("/dbinitialize");
