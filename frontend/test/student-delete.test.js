47c9a4e700a40cc6f0a6445f723424f038d2a90c
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2024-08-07 15:37:15 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -2,17 +2,17 @@ import { Selector } from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Student UI`
-    .page`http://localhost:3000/student`
+    .page`http://localhost:4401/student`
 
 test('Testing delete students', async t => {
-    await t.navigateTo("/test/addStudent");
+    await t.navigateTo("/addStudent");
     await t.typeText("#student-id", "222222");
     await t.typeText("#student-name", "Hiruni Gajanayake");
     await t.typeText("#student-age", "45");
     await t.typeText("#student-Hometown", "buddhist");
     await t.click("#student-add");
 
-    await t.navigateTo("/test/student");
+    await t.navigateTo("/student");
 
     await t.click("#student-delete-222222");
 
