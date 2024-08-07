75d78c549c7a6af80d0d7c9b52659af2815c7a26
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2024-08-07 13:13:26 +0530

    Update teacher-edit.test.js

--- frontend/test/teacher-edit.test.js
+++ frontend/test/teacher-edit.test.js
@@ -2,16 +2,16 @@ import {Selector} from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Teacher UI`
-    .page`http://localhost:8080`
+    .page`http://localhost:3000`
 test('Testing edit teachers', async t => {
-    await t.navigateTo("/api/teacher");
+    await t.navigateTo("/test/teacher");
     await t.click("#teacher-edit-10003");
 
     await t.typeText("#teacher-name", "Changed Teacher Name");
     await t.typeText("#teacher-age", "99");
     await t.click("#teacher-edit");
 
-    await t.navigateTo("/api/teacher");
+    await t.navigateTo("/test/teacher");
 
     const table = Selector('#teacher-table')
     const rowCount = await table.find('tr').count;
