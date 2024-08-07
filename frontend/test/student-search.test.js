b35ada68730c3e3a0529ea6581999f2a200ec9d6
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2024-08-07 15:36:26 +0530

    Update student-search.test.js

--- frontend/test/student-search.test.js
+++ frontend/test/student-search.test.js
@@ -2,10 +2,10 @@ import { Selector } from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Student UI`
-    .page`http://localhost:3000/student`
+    .page`http://localhost:4401/student`
 
 test('Testing search students', async t => {
-    await t.navigateTo("/test/student");
+    await t.navigateTo("/student");
     await t.typeText("#student-search", "si");
 
     const table = Selector('#student-table')
@@ -14,5 +14,5 @@ test('Testing search students', async t => {
     let tdText = await table.find('tr').nth(rowCount-1).innerText;
     await t.expect(rowCount).eql(2)
 
-    await t.navigateTo("/test/dbinitialize");
+    await t.navigateTo("/dbinitialize");
 });
