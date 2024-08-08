5ef6cb1f8c0bde3ada02f051aef9dac57e10739a
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2024-08-08 12:44:46 +0530

    Update teacher-add.test.js

--- frontend/test/teacher-add.test.js
+++ frontend/test/teacher-add.test.js
@@ -2,18 +2,18 @@ import {Selector} from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Teacher UI`
-    .page`http://localhost:8080/teacher`
+    .page`http://localhost:4200/student`;
 
 test('Testing add teachers', async t => {
     await t.navigateTo("/dbinitialize");
 
-    await t.navigateTo("/test/addTeacher");
+    await t.navigateTo("/addTeacher");
     await t.typeText("#teacher-id", "123456");
     await t.typeText("#teacher-name", "Mohan Perera");
     await t.typeText("#teacher-age", "45");
     await t.click("#teacher-add");
 
-    await t.navigateTo("/test/");
+    await t.navigateTo("teacher");
 
     const table = Selector('#teacher-table')
     const rowCount = await table.find('tr').count;
