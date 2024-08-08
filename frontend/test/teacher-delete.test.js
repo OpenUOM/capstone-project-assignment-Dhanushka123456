5e1e5138137d06e3fc5c2c7284d711059a7b8813
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2024-08-08 12:47:09 +0530

    Update teacher-delete.test.js

--- frontend/test/teacher-delete.test.js
+++ frontend/test/teacher-delete.test.js
@@ -2,16 +2,16 @@ import {Selector} from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Teacher UI`
-    .page`http://localhost:8080/teacher`
+    .page`http://localhost:4200/teacher`;
 
 test('Testing delete teachers', async t => {
-    await t.navigateTo("/test/addTeacher");
+    await t.navigateTo("/addTeacher");
     await t.typeText("#teacher-id", "300000");
     await t.typeText("#teacher-name", "Hasitha Fernando");
     await t.typeText("#teacher-age", "45");
     await t.click("#teacher-add");
 
-    await t.navigateTo("/test/");
+    await t.navigateTo("/teacher");
 
     await t.click("#teacher-delete-300000");
 
