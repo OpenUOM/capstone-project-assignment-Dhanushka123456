56498f6525ca488d7670ca452170641af683bd7f
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2024-12-05 22:59:49 +0530

    Update teacher-edit.test.js

--- frontend/test/teacher-edit.test.js
+++ frontend/test/teacher-edit.test.js
@@ -2,7 +2,7 @@ import {Selector} from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Teacher UI`
-    .page`http://localhost:3000/teacher`;
+    .page`http://localhost:8080/teacher`;
 test('Testing edit teachers', async t => {
     await t.navigateTo("/addTeacher");
     await t.click("#teacher-edit-10003");
