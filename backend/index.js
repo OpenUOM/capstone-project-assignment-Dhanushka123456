79afa922c5dcd84e8e29045cdc590845b2a8a92b
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-01-04 23:09:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 import server from "./server.js";
 
 if(process.env.NODE_ENV === "test"){
-  server.listen(3000, function () {
-    console.log(
-      "Capstone Project Backend is running on http://localhost:3000"
-    );
-  });
-}else{
   server.listen(8080, function () {
     console.log(
       "Capstone Project Backend is running on http://localhost:8080"
+    );
+  });
+}else{
+  server.listen(3000, function () {
+    console.log(
+      "Capstone Project Backend is running on http://localhost:3000"
     );
   });
 }
