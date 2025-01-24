3fc82e673dd571b3c43ac6f43aefd21855ee502a
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-01-24 22:17:46 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -5,18 +5,16 @@ fixture`Testing Student UI`
     .page`http://localhost:8080/student`;
     
 test('Testing delete students', async t => {
-    await t.navigateTo("/dbinitialize");
     await t.navigateTo("/addStudent");
-    await t.typeText("#student-id", "20003");
+    await t.typeText("#student-id", "222222");
     await t.typeText("#student-name", "Hiruni Gajanayake");
     await t.typeText("#student-age", "45");
-    await t.typeText("#student-Hometown", "buddhist");
+    await t.typeText("#student-religion", "buddhist");
     await t.click("#student-add");
 
     await t.navigateTo("/student");
-    await t.navigateTo("/deleteStudent");
-    
-    await t.click("#student-delete", "20003");
+
+    await t.click("#student-delete-222222");
 
     const table = Selector('#student-table')
     const rowCount = await table.find('tr').count;
