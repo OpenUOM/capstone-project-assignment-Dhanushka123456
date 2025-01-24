db28d32eb88c1700f16f43fb5f81e9dbfd3f1dd7
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-01-24 22:18:53 +0530

    Update student-edit.test.js

--- frontend/test/student-edit.test.js
+++ frontend/test/student-edit.test.js
@@ -6,19 +6,16 @@ fixture`Testing Student UI`
 
 test('Testing edit students', async t => {
     await t.navigateTo("/addStudent");
-    await t.typeText("#student-id", "20004");
-    await t.typeText("#student-name", "Pasindu Basnayaka");
-    await t.typeText("#student-age", "45");
-    await t.typeText("#student-Hometown", "catholic");
+    await t.typeText("#student-id", "222222");
+    await t.typeText("#student-name", "Isuri De Silva");
+    await t.typeText("#student-age", "27");
+    await t.typeText("#student-religion", "buddhist");
     await t.click("#student-add");
 
-    await t.navigateTo("/student");
-    await t.click("#student-edit");
-    
-    await t.typeText("#student-id", "20005");
-    await t.typeText("#student-name", "Somadasa kasun");
-    await t.typeText("#student-age", "99");
-    await t.typeText("#student-Hometown", "Hometown");
+    await t.navigateTo("/editStudent");
+    await t.typeText("#student-name", "Isuri Sugathadasa");
+    await t.typeText("#student-age", "28");
+    await t.typeText("#student-religion", "catholic");
     await t.click("#student-edit");
 
     await t.navigateTo("/student");
@@ -27,8 +24,5 @@ test('Testing edit students', async t => {
     const rowCount = await table.find('tr').count;
 
     let tdText = await table.find('tr').nth(rowCount - 1).innerText;
-    await t.expect(tdText).contains("Somadasa kasun");
-
-    await t.navigateTo("/student");
-    await t.click("#student-delete-20005");
+    await t.expect(tdText).contains("Isuri Sugathadasa");
 });
