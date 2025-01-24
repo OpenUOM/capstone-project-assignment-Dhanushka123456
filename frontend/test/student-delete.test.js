57d61dc7f023434d83400d8f801f9097524f5e0d
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-01-24 21:43:10 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -14,15 +14,15 @@ test('Testing delete students', async t => {
 
     await t.navigateTo("/student");
 
-    await t.typeText("#student-id", "20004");
-    await t.typeText("#student-name", "Pasindu Basnayaka");
+    await t.typeText("#student-id", "20003");
+    await t.typeText("#student-name", "Hiruni Gajanayake");
     await t.typeText("#student-age", "45");
-    await t.typeText("#student-Hometown", "Kegalla");
+    await t.typeText("#student-Hometown", "buddhist");
     await t.click("#student-delete");
 
     const table = Selector('#student-table')
     const rowCount = await table.find('tr').count;
 
     let tdText = await table.find('tr').nth(rowCount - 1).innerText;
-    await t.expect(tdText).notContains("Pasindu Basnayaka");
+    await t.expect(tdText).notContains("Hiruni Gajanayake");
 });
