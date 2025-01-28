5f2dfd4115fb2052726f062627934b7f1d9e8bbb
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-01-28 22:19:39 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -7,7 +7,7 @@ fixture`Testing Student UI`
 test('Testing delete students', async t => {
     await t.navigateTo("/dbinitialize");
     await t.navigateTo("/addStudent");
-    await t.typeText("#student-id", "222222");
+    await t.typeText("#student-id", "20004");
     await t.typeText("#student-name", "Hiruni Gajanayake");
     await t.typeText("#student-age", "45");
     await t.typeText("#student-Hometown", "buddhist");
@@ -18,16 +18,14 @@ test('Testing delete students', async t => {
      await t.navigateTo("/dbinitialize");
     await t.navigateTo("/deleteStudent");
     
-    await t.typeText("#student-name", "Hiruni Gajanayake");
-    await t.typeText("#student-age", "45");
-    await t.typeText("#student-Hometown", "buddhist");
+    
     await t.click("#student-delete");
 
-    await t.click("#student-delete-222222");
+    await t.click("#student-delete-20004");
     
     const table = Selector('#student-table')
     const rowCount = await table.find('tr').count;
 
     let tdText = await table.find('tr').nth(rowCount - 1).innerText;
-    await t.expect(tdText).notContains("Pasindu Basnayaka");
+    await t.expect(tdText).notContains("Hiruni Gajanayake");
 });
