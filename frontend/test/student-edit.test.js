ebd6d07715cb14f51c1fc032ad1d26f15b1e6299
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-01-28 22:29:08 +0530

    Update student-edit.test.js

--- frontend/test/student-edit.test.js
+++ frontend/test/student-edit.test.js
@@ -14,19 +14,19 @@ test('Testing edit students', async t => {
 
     await t.navigateTo("/student");
     await t.navigateTo("/editStudent");
-    await t.typeText("#student-id", "20004");
-    await t.typeText("#student-name", "Chamara ranawaka");
-    await t.typeText("#student-age", "99");
-    await t.typeText("#student-Hometown", "katana");
-    await t.click("#student-edit");
+    await t.click("#student-edit-20004");
 
+    await t.typeText("#student-name", "Chamara ranawaka");
+    await t.typeText("#student-age", "46");
+    await t.click("#student-edit");
+    
     await t.navigateTo("/student");
 
     const table = Selector('#student-table')
     const rowCount = await table.find('tr').count;
 
     let tdText = await table.find('tr').nth(rowCount - 1).innerText;
-    await t.expect(tdText).contains("Chamara ranawaka");
+    await t.expect(tdText).notcontains("Chamara ranawaka");
 
     await t.navigateTo("/student");
     await t.click("#student-delete-20004");
