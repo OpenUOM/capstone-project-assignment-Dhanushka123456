3d469e6adfb78c9435199493a6f40b0358e7b74d
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-02-02 12:13:14 +0530

    Update teacher-edit.test.js

--- frontend/test/teacher-edit.test.js
+++ frontend/test/teacher-edit.test.js
@@ -2,9 +2,13 @@ import {Selector} from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Teacher UI`
-    .page`http://localhost:8080/teacher`;
+    .page`http://localhost:8080/`;
 test('Testing edit teachers', async t => {
     await t.navigateTo("/addTeacher");
+    await t.typeText("#teacher-id", "10003");
+    await t.typeText("#teacher-name", "Hasitha Fernando");
+    await t.typeText("#teacher-age", "45");
+    await t.click("#teacher-add");
     await t.click("#teacher-edit-10003");
 
     await t.typeText("#teacher-name", "Sadhamali thushitha");
