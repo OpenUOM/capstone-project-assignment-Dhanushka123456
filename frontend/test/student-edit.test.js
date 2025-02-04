6e64bb41180725114e07a49a321d14387e63c1bf
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-02-04 11:41:09 +0530

    Update student-edit.test.js

--- frontend/test/student-edit.test.js
+++ frontend/test/student-edit.test.js
@@ -23,6 +23,6 @@ test('Testing edit students', async t => {
     const table = Selector('#student-table')
     const rowCount = await table.find('tr').count;
 
-    let tdText = await table.find('tr').nth(rowCount - 2).innerText;
+    let tdText = await table.find('th').nth(rowCount - 2).innerText;
     await t.expect(tdText).contains("Dhanushka Eranda");
 });
