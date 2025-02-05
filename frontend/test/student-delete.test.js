df68d78875e8a8945f9cea4fd4c609b64b9e9121
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-02-05 23:05:32 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -13,13 +13,13 @@ test('Testing delete students', async t => {
     await t.click("#student-add");
 
     await t.navigateTo("/student");
-    const deleteButton = Selector('[id^="student-delete-"]').withText("20004");
-    await t.click(deleteButton);
+    //const deleteButton = Selector('[id="student-delete"]').withText("20004");
+    //await t.click(deleteButton);
 
-    await t.wait(1000);
+    //await t.wait(1000);
 
 
-    //await t.click("#student-delete-20004");
+    await t.click("#student-delete"="20004");
     //await t.click(Selector("td[role='gridcell'] span").nth(33));    
     
     const table = Selector('#student-table')
