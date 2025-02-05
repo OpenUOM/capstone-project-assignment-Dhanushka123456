888df39d03a06c919ea76e69ac8ffbcd0acf51fa
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-02-05 23:38:59 +0530

    Update teacher-delete.test.js

--- frontend/test/teacher-delete.test.js
+++ frontend/test/teacher-delete.test.js
@@ -13,7 +13,7 @@ test('Testing delete teachers', async t => {
 
     await t.navigateTo("/");
 
-    await t.click("#teacher-delete"["10003"]);
+    await t.click("#teacher-delete"["#teacher-id","10003"]);
     await t.navigateTo("/");
     const table = Selector('#teacher-table')
     const rowCount = await table.find('tr').count;
