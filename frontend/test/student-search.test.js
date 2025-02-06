e0e7e4d94421f9526f9b9794a3bf68ce4417b944
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-02-06 19:48:42 +0530

    Update student-search.test.js

--- frontend/test/student-search.test.js
+++ frontend/test/student-search.test.js
@@ -6,13 +6,13 @@ fixture`Testing Student UI`
 
 test('Testing search students', async t => {
     await t.navigateTo("/student");
-    await t.typeText("#student-search", "si");
+    await t.click("#student-search");
 
     const table = Selector('#student-table')
     const rowCount = await table.find('tr').count;
 
     let tdText = await table.find('tr').nth(rowCount-1).innerText;
-    await t.expect(rowCount).eql(-2)
+    await t.expect(rowCount).eql(-1)
 
     await t.navigateTo("/dbinitialize");
 });
