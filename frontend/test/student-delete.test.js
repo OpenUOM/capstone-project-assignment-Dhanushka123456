c26480216ab14ef7befb784d2aeb58b6e85562ce
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-02-06 19:20:36 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -17,8 +17,8 @@ test('Testing delete students', async t => {
     await t.click("#student-delete-20004");
     await t.navigateTo("/student");
     const table = Selector('#student-table')
-    const rowCount = await table.find('tr').count;
+    const rowCount = await table.find('td').count;
 
-    let tdText = await table.find('tr').nth(rowCount - 1).innerText;
+    let tdText = await table.find('td').nth(rowCount - 1).innerText;
     await t.expect(tdText).notContains("20004");
 });
