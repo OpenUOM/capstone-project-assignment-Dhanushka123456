1ad38c446cd7dfb7e14a9c1615325211a21b5927
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-02-08 10:37:54 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -5,18 +5,8 @@ fixture`Testing Student UI`
     .page`http://localhost:8080/student`;
     
 test('Testing delete students', async t => {
-    await t.navigateTo("/addStudent");
-    await t.typeText("#student-id", "20005");
-    await t.typeText("#student-name", "Hiruni Gajanayake");
-    await t.typeText("#student-age", "45");
-    await t.typeText("#student-Hometown", "Kandy");
-    await t.click("#student-add");
-    
     await t.navigateTo("/deleteStudent");
-    await t.typeText("#student-name", "Hiruni Gajanayake");
-    await t.typeText("#student-age", "45");
-    await t.typeText("#student-Hometown", "Kandy");
-    await t.click("#student-delete");
+    await t.click("#student-delete-20003");
     //const studentRow = Selector('#student-table').find('td').withText("20005");
    // await t.expect(studentRow.exists).ok('Student should be present before deletion');
 
@@ -30,7 +20,7 @@ test('Testing delete students', async t => {
     const rowCount = await table.find('tr').count;
 
     let tdText = await table.find('td').nth(rowCount - 1).innerText;
-    await t.expect(tdText).contains("20005");
+    await t.expect(tdText).contains("20003");
 
     
 });
