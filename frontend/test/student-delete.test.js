25ecf37c7e4912af193c8f449e3e30c1943febe3
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-02-08 11:24:57 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -14,7 +14,7 @@ test('Testing delete students', async t => {
 
     await t.navigateTo("/deleteStudent");
     await t.typeText("id", "20008");
-    await t.click("#student-delete);
+    await t.click("#student-delete-20008");
     
     await t.navigateTo("/student");
     const table = Selector('#student-table')
