0c85d14998ed7a2d57f234e7b1decb0a3f683e82
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-02-09 10:24:36 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -5,14 +5,14 @@ fixture`Testing Student UI`
     .page`http://localhost:8080/student`
 
 test('Testing delete students', async t => {
-    //await t.navigateTo("/addStudent");
-    //await t.typeText("#student-id", "20003");
-    //await t.typeText("#student-name", "Isuri De Silva");
-    //await t.typeText("#student-age", "10");
-   // await t.typeText("#student-Hometown", "Kandy");
-    //await t.click("#student-add");
-
     await t.navigateTo("/addStudent");
+    await t.typeText("#student-id", "20003");
+    await t.typeText("#student-name", "Isuri De Silva");
+    await t.typeText("#student-age", "10");
+    await t.typeText("#student-Hometown", "Kandy");
+    await t.click("#student-add");
+
+    //await t.navigateTo("/addStudent");
     await t.click("#student-delete-20003");
 
     await t.navigateTo("/student");
