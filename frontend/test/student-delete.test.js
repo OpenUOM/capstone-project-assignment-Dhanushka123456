4b57a7e9c48a93ebbb0a02d6847d9f694c1ecad9
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-02-09 09:46:18 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -13,7 +13,7 @@ test('Testing delete students', async t => {
     await t.click("#student-add");
 
     await t.navigateTo("/deleteStudent");
-    await t.click(`#student-delete-${`20003`}`);
+    await t.click(`#student-delete-${`id`}`);
 
     //await t.click(`#student-delete-${20003}`);
     await t.navigateTo("/student");
