24baf4015cf24c0c4361967ef3325be4f4a62f2d
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-02-09 10:08:20 +0530

    Update test.js

--- backend/test/test.js
+++ backend/test/test.js
@@ -181,9 +181,7 @@ describe("Student Endpoints", () => {
     // delete Student
     await requestWithSupertest.post("/deleteStudent").send({
       "id": 20003,
-      "name": "Sandakan",
-      "age": 15,
-      "hometown": "Homagama"
+      
     });
 
     const res = await requestWithSupertest.get("/listStudents");
