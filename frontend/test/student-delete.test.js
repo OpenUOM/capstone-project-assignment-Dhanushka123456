637fc81c6f10c4db74687503b82d4f756edfdc28
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-02-09 12:33:38 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -13,8 +13,12 @@ test('Testing delete students', async t => {
    await t.click("#student-add");
     
     await t.navigateTo("/deleteStudent");
-    await t.typeText("#student-id", "#student-delete-20003");
-    await t.click("#student-delete");
+    //await t.typeText("#student-id", "#student-delete-20003");
+    //await t.click("#student-delete");
+
+    await t.expect(Selector("#student-delete-20003").exists).ok();
+    await t.expect(Selector("#student-delete-20003").visible).ok();
+    await t.click("#student-delete-20003");
     
 
     const table = Selector('#student-table')
