fc62cd02373a0816dd178a9edca60b9647f4969c
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-02-27 23:43:45 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -5,27 +5,20 @@ fixture`Testing Student UI`
     .page`http://localhost:8080/student`
 
 test('Testing delete students', async t => {
-   // await t.navigateTo("/addStudent");
-   // await t.typeText("#student-id", "20003");
-    //await t.typeText("#student-name", "Isuri De Silva");
-    //await t.typeText("#student-age", "10");
-    //await t.typeText("#student-Hometown", "Kandy");
-   // await t.click("#student-add");
+    await t.navigateTo("/addStudent");
+    await t.typeText("#student-id", "222222");
+    await t.typeText("#student-name", "Hiruni Gajanayake");
+    await t.typeText("#student-age", "45");
+    await t.typeText("#student-religion", "buddhist");
+    await t.click("#student-add");
 
-    await t.navigateTo("/students");
-    await t.typeText("#student-id","20003");
-    await t.click("#student-delete");
-    
+    await t.navigateTo("/student");
 
-    //const deleteButton = Selector("#student-delete-20003");
-    //await t.click(deleteButton);
+    await t.click("#student-delete-222222");
 
-    //await t.navigateTo("/student");
-
-    const table = Selector("/students");
+    const table = Selector('#student-table')
     const rowCount = await table.find('tr').count;
 
-    let lastRowText = await table.find('td').nth(rowCount - 1).innerText;
-    await t.expect(lastRowText).notContains("20003");
-    //await t.click("#student-delete");
+    let tdText = await table.find('tr').nth(rowCount - 1).innerText;
+    await t.expect(tdText).notContains("Hiruni Gajanayake");
 });
