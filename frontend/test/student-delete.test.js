e3aa8191543d8fb4edacf3abf376bfd3d6e30d73
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-03-06 22:08:10 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -2,32 +2,23 @@ import { Selector } from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Student UI`
-    .page`http://localhost:8080/student`;
+    .page`http://localhost:8080/student`
 
 test('Testing delete students', async t => {
-    // Step 1: Navigate to the add student page and add a new student
     await t.navigateTo("/addStudent");
     await t.typeText("#student-id", "555555");
-    await t.typeText("#student-name", "Kusal Mendis");
+    await t.typeText("#student-name", "kusal Mendis");
     await t.typeText("#student-age", "35");
     await t.typeText("#student-Hometown", "Kurunegala");
     await t.click("#student-add");
-
-    // Step 2: Check that the student has been added
-    await t.navigateTo("/student");
-    const table = Selector('#student-table');
-    await t.expect(table.innerText).notContains("Kusal Mendis");
-
-    // Step 3: Navigate to delete page and delete the added student
-    await t.navigateTo("/deleteStudent");
-    const deleteButton = Selector(`#student-delete-555555`);
     
-    await t.click(deleteButton);
-    
-    // Step 4: Navigate back to the student list page
     await t.navigateTo("/student");
+    await t.click("#student-delete");
+    
+    await t.navigateTo("/student");
+    const table = Selector('#student-table')
+    const rowCount = await table.find('tr').count;
 
-    // Step 5: Ensure the student has been deleted
-    const updatedTable = Selector('#student-table');
-    await t.expect(updatedTable.innerText).notContains("Kusal Mendis");
+    let tdText = await table.find('tr').nth(rowCount - 1).innerText;
+    await t.expect(tdText).contains("555555");
 });
