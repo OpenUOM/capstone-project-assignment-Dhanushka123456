3f1b54dd316373a0e1c093da79a580be7764644c
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-03-07 23:59:57 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -5,39 +5,20 @@ fixture`Testing Student UI`
     .page`http://localhost:8080/student`
 
 test('Testing delete students', async t => {
-    await t.navigateTo("/dbinitialize");
-
     await t.navigateTo("/addStudent");
-    await t.typeText("#student-id", "20004");
-    await t.typeText("#student-name", "Pasindu Basnayaka");
+    await t.typeText("#student-id", "222222");
+    await t.typeText("#student-name", "Hiruni Gajanayake");
     await t.typeText("#student-age", "45");
-    await t.typeText("#student-Hometown", "Kegalla");
+    await t.typeText("#student-religion", "buddhist");
     await t.click("#student-add");
 
     await t.navigateTo("/student");
 
-    const table = Selector('#student-table')
+    await t.click("#deleteStudent");
+
+    const table = Selector('#student-delete-222222')
     const rowCount = await table.find('tr').count;
 
     let tdText = await table.find('tr').nth(rowCount - 1).innerText;
-    await t.expect(tdText).notContains("Pasindu Basnayaka");
-
-    //const studentId = 20004; // Fetch the student ID
-
-// Navigate to the student URL
-    await t.navigateTo("/student/id");
-
-// Click the delete button for the specific student
-    await t.click("#student-delete-20004");
-
-   
-    await t.navigateTo("/student");
-    const updatedRowCount = await table.find('tr').count;
-
-    await t.expect(updatedRowCount).eql(rowCount - 1);
-
- 
-    let isStudentPresent = await table.innerText;
-    await t.expect(isStudentPresent).notContains("Pasindu Basnayaka");
+    await t.expect(tdText).notContains("Hiruni Gajanayake");
 });
-    
