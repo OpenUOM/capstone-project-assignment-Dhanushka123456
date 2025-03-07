a00b5c087710d9066985df9163aba023dcad56fd
Dhanushka123456 <111176779+Dhanushka123456@users.noreply.github.com>
2025-03-08 00:22:19 +0530

    Update teacher-delete.test.js

--- frontend/test/teacher-delete.test.js
+++ frontend/test/teacher-delete.test.js
@@ -2,22 +2,22 @@ import {Selector} from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Teacher UI`
-    .page`http://localhost:8080/`;
+    .page`http://localhost:8080/teacher`;
 
-test('Testing delete teachers', async t => {
+test('Testing add teachers', async t => {
+    await t.navigateTo("/dbinitialize");
+
     await t.navigateTo("/addTeacher");
-    await t.typeText("#teacher-id", "300000");
-    await t.typeText("#teacher-name", "Hasitha Fernando");
+    await t.typeText("#teacher-id", "10004");
+    await t.typeText("#teacher-name", "Mohan Perera");
     await t.typeText("#teacher-age", "45");
     await t.click("#teacher-add");
 
     await t.navigateTo("/");
 
-    await t.click("#teacher-delete-300000");
-
     const table = Selector('#teacher-table')
-    const rowCount = await table.find('td').count;
+    const rowCount = await table.find('tr').count;
 
-    let tdText = await table.find('td').nth(rowCount - 1).innerText;
-    await t.expect(tdText).notContains("Hasitha Fernando");
+    let tdText = await table.find('tr').nth(rowCount - 1).innerText;
+    await t.expect(tdText).notContains("Mohan Perera");
 });
